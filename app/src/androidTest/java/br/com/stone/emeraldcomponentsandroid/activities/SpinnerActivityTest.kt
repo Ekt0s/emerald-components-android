d7e191d718d18e870a84197b90a306cb24e021a3
diegolucasb <diegolucasb@gmail.com>
2018-09-25 17:36:39 -0300

    :art: Adds Spoon screenshot implementation

--- app/src/androidTest/java/br/com/stone/emeraldcomponentsandroid/activities/SpinnerActivityTest.kt
+++ app/src/androidTest/java/br/com/stone/emeraldcomponentsandroid/activities/SpinnerActivityTest.kt
@@ -1,5 +1,6 @@
 package br.com.stone.emeraldcomponentsandroid.activities
 
+import android.Manifest
 import android.support.test.espresso.Espresso.onData
 import android.support.test.espresso.Espresso.onView
 import android.support.test.espresso.action.ViewActions.click
@@ -12,9 +13,12 @@ import android.support.test.espresso.matcher.ViewMatchers.withId
 import android.support.test.espresso.matcher.ViewMatchers.withSpinnerText
 import android.support.test.espresso.matcher.ViewMatchers.withText
 import android.support.test.rule.ActivityTestRule
+import android.support.test.rule.GrantPermissionRule
 import android.support.test.runner.AndroidJUnit4
 import android.widget.EditText
 import br.com.stone.emeraldcomponentsandroid.R
+import com.jraska.falcon.FalconSpoonRule
+import com.squareup.spoon.SpoonRule
 import org.hamcrest.Matchers.`is`
 import org.hamcrest.Matchers.instanceOf
 import org.hamcrest.Matchers.not
@@ -31,6 +35,15 @@ import org.junit.runner.RunWith
  */
 @RunWith(AndroidJUnit4::class)
 class SpinnerActivityTest {
+
+    @get:Rule
+    val permission: GrantPermissionRule = GrantPermissionRule.grant(
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.READ_EXTERNAL_STORAGE)
+
+    @get:Rule
+    val spoon = FalconSpoonRule()
+
     @get:Rule
     val activityRule = ActivityTestRule(SpinnerActivity::class.java)
 
@@ -40,12 +53,18 @@ class SpinnerActivityTest {
                 ViewMatchers.isDescendantOfA(withId(R.id.emeraldAutoComplete)),
                 ViewMatchers.isAssignableFrom(EditText::class.java)))
 
+        spoon.screenshot(activityRule.activity, "Starting")
+
         val query = "100 - Aaaaaaaaa"
         autoCompleteView.perform(click())
         autoCompleteView.perform(replaceText("100"), closeSoftKeyboard())
 
+        spoon.screenshot(activityRule.activity, "Type-100-on-autoComplete")
+
         onView(withText(query)).inRoot(withDecorView(not(`is`(activityRule.activity.window.decorView))))
                 .perform(click())
+
+        spoon.screenshot(activityRule.activity, "Clicked-on-suggestion-window")
 
         autoCompleteView.check(matches(withText(query)))
     }
@@ -55,8 +74,12 @@ class SpinnerActivityTest {
         onView((withId(R.id.emeraldSpinner)))
                 .perform(click())
 
+        spoon.screenshot(activityRule.activity, "Clicked-on-spinner")
+
         val expectedOption = "Option 2"
         onData(allOf(`is`(instanceOf(String::class.java)), `is`(expectedOption))).perform(click())
         onView((withId(R.id.emeraldSpinner))).check(matches(withSpinnerText(expectedOption)))
+
+        spoon.screenshot(activityRule.activity, "Clicked-on-second-option")
     }
 }
